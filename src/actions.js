ba7251a026175999bf3de97b14834c5ef6864d71
Dannie Vinther <davi@kea.dk>
2024-11-25 07:44:51 +0100

    Refactor newsletter subscription handling with improved error management and UI feedback

---
+++ src/actions.js
@@ -0,0 +1,35 @@
+"use server";
+
+import { addSub } from "@/lib/api";
+import { revalidatePath } from "next/cache";
+
+export async function actionSubmit(prev, formData) {
+  const data = {
+    email: formData.get("email"),
+    name: formData.get("name"),
+  };
+  const errors = {};
+
+  if (!data.name) {
+    errors.name = "Name is required";
+  }
+  if (!data.email) {
+    errors.email = "Email is required";
+  }
+
+  if (errors.email.length > 0 && errors.name.length > 0) {
+    return { success: false, errors };
+  }
+  // if (Object.keys(errors).length > 0) {
+  //   return { errors };
+  // }
+
+  const res = await addSub(data);
+
+  if (res) {
+    revalidatePath("/");
+    return { success: true, message: "Thanks for submitting" };
+  } else {
+    return { success: false, message: "Failed to submit" };
+  }
+}
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,23 +1,15 @@
-import { addSub } from "@/lib/api";
-import { revalidatePath } from "next/cache";
+"use client";
+
+import { useActionState } from "react";
+import { actionSubmit } from "@/actions";
+import SubmitButton from "./SubmitButton";
 
 function Newsletter() {
-  async function actionSubmit(formData) {
-    "use server";
-    const data = {
-      email: formData.get("email"),
-      name: formData.get("name"),
-    };
-    const res = await addSub(data);
-    if (res) {
-      console.log(res[0].id);
-      revalidatePath("/");
-    }
-  }
+  const [state, formAction] = useActionState(actionSubmit, undefined);
 
   return (
     <form
-      action={actionSubmit}
+      action={formAction}
       className="max-w-md mx-auto p-4 bg-white shadow-md rounded"
     >
       <div className="mb-4">
@@ -30,6 +22,9 @@ function Newsletter() {
           name="name"
           className="w-full px-3 py-2 border border-gray-300 rounded"
         />
+        <p className="bg-red-100 text-red-950">
+          {state?.errors && state.errors.name}
+        </p>
       </div>
       <div className="mb-4">
         <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
@@ -41,13 +36,14 @@ function Newsletter() {
           name="email"
           className="w-full px-3 py-2 border border-gray-300 rounded"
         />
+        <p className="bg-red-100 text-red-950">
+          {state?.errors && state.errors.email}
+        </p>
       </div>
-      <button
-        type="submit"
-        className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-      >
-        Subscribe
-      </button>
+      <SubmitButton />
+      {state?.errors && state.errors.submit && (
+        <p className="text-red-500 text-sm mt-2">{state.errors.submit}</p>
+      )}
     </form>
   );
 }
